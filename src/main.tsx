cc97492ab62b2f457819316013683cc615741639
Apoorv Gupta <apoorv2766@outlook.com>
2025-08-22 17:11:25 +0530

    final changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { CssBaseline } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
     <BrowserRouter basename="/sign_signup_validation">
+      <CssBaseline />
       <App />
     </BrowserRouter>
-  </React.StrictMode>
 );
