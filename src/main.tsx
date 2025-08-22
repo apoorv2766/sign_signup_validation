09ae973db88f777620dcf68eb86c6d6b5d4f4b67
Apoorv Gupta <apoorv2766@outlook.com>
2025-08-22 15:52:47 +0530

    added singup link not working

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 import App from "./App";
 import { CssBaseline } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CssBaseline />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
